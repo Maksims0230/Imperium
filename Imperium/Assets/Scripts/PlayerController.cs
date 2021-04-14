b98c7281e374043343f7031757e9249dbcbdfcd1
Maksims0230 <isip_m.a.stepankov@mpt.ru>
2021-04-14 16:27:49 +0300

    update2

---
+++ Imperium/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 0.0f;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    // Start is called before the first frame update
+    void Start() => rb = GetComponent<Rigidbody2D>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * speed;
+    }
+
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
